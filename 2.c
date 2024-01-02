bbb94d0a27344fde0c70ac8e6325cabc88d271f5
achyutskhot0 <152766127+achyutskhot0@users.noreply.github.com>
2024-01-02 14:08:00 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-int length(char s);
+int length(char s[40]);
 
 void main(){
     char str[40];
     char *s[40] = &str;
     printf("Enter a string\n");
     scanf("%s", str);
+    int len = length (s);
+    printf("The length of the given strong is %d", len);
     
-    
+}
+length(char s[40]){
+    int length= 0;
+    for(int i= 0; &s[i]!= '\0'; i++){
+        length++;
+    }
+    return length;
 }
